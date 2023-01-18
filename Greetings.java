9170fe4ff7c4012e43afb6e540fff2c68a785dd1
SJGordon <shamar101s@icloud.com>
2023-01-17 19:27:20 -0500

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,16 @@
+// Shamar Gordon 
+// 1/30/2023
+// Assignment 1 Greetings
+
+
+
+// This program will print a greeting to the screen
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println ();
+        System.out.println ("Good morning, Lab intructor Bin Li.");
+        System.out.println ();
+        System.out.println ("I am Shamar Gordon.");
+        System.out.println ();
+    }
+}
