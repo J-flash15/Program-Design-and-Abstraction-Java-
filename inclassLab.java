fe07e06c29fcce515b5fcfef2d4929e83c33d47e
SJGordon <shamar101s@icloud.com>
2023-01-30 12:37:47 -0500

    Add files via upload

---
+++ inclassLab.java
@@ -0,0 +1,72 @@
+public class inclassLab {
+    public static void main(String[] args) {
+        firstinitialBlock();
+        secondinitialBlock();
+        thirdinitialBlock();
+        forthinitialBlock();
+        fifthinitialBlock();
+        sixthinitialBlock();
+    }
+
+    public static void firstinitialBlock() {
+        System.out.println("  _____");
+        System.out.println(" / ____|");
+        System.out.println(" \\___  \\");
+        System.out.println(" |_____/ ");
+    }
+
+    public static void secondinitialBlock() {
+
+        System.out.println("\n ");
+        System.out.println("  ||  ||");
+        System.out.println("  ||--||");
+        System.out.println("  ||  ||");
+        
+
+    }
+
+    public static void thirdinitialBlock() {
+
+        System.out.println(" \n");
+        System.out.println("   _____");
+        System.out.println("  /  __ \\");
+        System.out.println(" /  /__\\ \\");
+        System.out.println("/  /____\\ \\"); 
+    }
+
+
+    public static void forthinitialBlock() {
+
+        System.out.println(" \n");
+        System.out.println("  _________");
+        System.out.println(" |_________|");
+        System.out.println("  //  ||  \\\\");
+        System.out.println(" //   ||   \\\\ ");
+      
+    }
+
+
+    public static void fifthinitialBlock() {
+        System.out.println(" \n");
+        System.out.println("   _____");
+        System.out.println("  /  __ \\");
+        System.out.println(" /  /__\\ \\");
+        System.out.println("/  /____\\ \\"); 
+
+    }
+
+    
+
+
+    public static void sixthinitialBlock() {
+        System.out.println(" \n");
+        System.out.println("  _____");
+        System.out.println(" /  ___\\");
+        System.out.println(" | /   //");
+        System.out.println(" | \\__/ \\");
+        System.out.println(" | |   \\ \\");
+    }
+
+   
+    
+}
